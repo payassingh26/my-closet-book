67bdf35ba5c01a2e20ec64ed881a17f7983a2fca
Payas Singh <170888934+payassingh26@users.noreply.github.com>
2025-08-15 19:51:47 -0400

    First commit

---
+++ src/main/java/com/example/myclosetbook/MyclosetbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.myclosetbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclosetbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyclosetbookApplication.class, args);
+	}
+
+}
