ed7ec4b345a2a09c18c2d303d5dc9f476f26a39d
Payas Singh <170888934+payassingh26@users.noreply.github.com>
2025-08-18 23:59:48 -0400

    add docker compose, dockerfile

--- src/main/java/com/example/myclosetbook/MyClosetBookApplication.java
+++ src/main/java/com/example/myclosetbook/MyClosetBookApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyclosetbookApplication {
+public class MyClosetBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyclosetbookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyClosetBookApplication.class, args);
+    }
 
 }
